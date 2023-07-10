e6b1e416a7990dd8a4616345562fe23ef4da26ba
The-Gae-Bolg <juan_america2014@hotmail.es>
2023-07-10 14:38:01 -0600

    404 page

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,6 +5,11 @@ export default {
     "./src/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
+    fontFamily: {
+      'Montserrat': ['Montserrat', 'ui-sans-serif', 'system-ui'],
+      'SpaceMono': ['Space Mono', 'ui-monospace', 'system-ui'],
+      'Inconsolata': ['Inconsolata', 'ui-monospace', 'system-ui'],
+    },
     extend: {},
   },
   plugins: [],
